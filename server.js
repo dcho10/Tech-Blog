08bf452913ea51b01d247c9204b8ca031787e86c
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-04-19 22:57:51 -0400

    Set up controllers, api, and user routes

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
